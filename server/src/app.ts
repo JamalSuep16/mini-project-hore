64a1ef3780a1ac5358be375831f9c2e4ce56569d
KaonashiMFG <dharmasenaakmalaji@outlook.com>
2025-01-21 13:37:38 +0700

    table and next js added

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,16 @@ app.get("/api/v1", (req: Request, res: Response) => {
   res.status(200).send("<h1>Welcome!</h1>");
 });
 
+app.get("/api/v1/posts", async (req: Request, res: Response) => {
+  try {
+    const events = await prisma.events.findMany(); 
+    res.status(200).json({ ok: true, data: events });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ ok: false, error: "Internal server error" });
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(Server is listening on port: ${PORT});
-  });
+  console.log(`Server is listening on port: ${PORT}`);
+});
